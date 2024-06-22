abb162e9023fdeaff43990fc687638d41d843268
Howe <121561565+donahowe@users.noreply.github.com>
2024-06-22 15:58:02 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -37,18 +37,18 @@ LARGE_CONSTANT = 520
 LARGE_CONSTANT2 = 5201314
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_path", default='/cache/demo.json', type=str, help="Dialogue instruction data path")
-parser.add_argument("--sd_path", default='/pretrained_models/diffusion_1.5', type=str, help="Path to Stable Diffusion Folder")
-parser.add_argument("--vae_path", default='/pretrained_models/vae_ft_mse', type=str, help="Path to VAE Folder")
-parser.add_argument("--repeats", default=20, type=int, help="Number of samples for each prompt")
+parser.add_argument("--data_path", default='cache/demo.json', type=str, help="Dialogue instruction data path")
+parser.add_argument("--sd_path", default='/data2/chengjunhao/THEATERGEN/pretrained_models/diffusion_1.5_comic', type=str, help="Path to Stable Diffusion Folder")
+parser.add_argument("--vae_path", default='/data2/chengjunhao/THEATERGEN/pretrained_models/vae_ft_mse', type=str, help="Path to VAE Folder")
+parser.add_argument("--repeats", default=2, type=int, help="Number of samples for each prompt")
 parser.add_argument("--seed_offset", default=1, type=int, help="Offset to the seed (seed starts from this number)")
-parser.add_argument("--sd_version", default='1.5plus', type=str, help="Base model version. Pick from [1.5, 1.5plus]") # 1.5, 1.5plus is recommended
-parser.add_argument("--save_dir", default='/output', type=str, help="Database path")
+parser.add_argument("--sd_version", default='xlplus', type=str, help="Base model version. Pick from [1.5, 1.5plus, xl, xlplus]") # 1.5, 1.5plus is recommended
+parser.add_argument("--save_dir", default='output', type=str, help="Database path")
 parser.add_argument("--freeze_dialogue_seed", default = 1, type=int, help="Use same seed for each dialogue for more consistency")
 parser.add_argument("--do_latent_guidance", default = True, type=bool, help="Latent initialization for each character")
 parser.add_argument("--is_editing", default = False, type=bool, help="Multi-turn editing mode")
 parser.add_argument("--is_CMIGBENCH", default = False, type=bool, help="Multi-turn editing mode")
-parser.add_argument("--device", default = 'cuda', type=str, help="Run Device")
+parser.add_argument("--device", default ='cuda', type=str, help="Run Device")
 
 
 args = parser.parse_args()
@@ -107,7 +107,7 @@ elif args.sd_version == '1.5plus':
     )   
     
     vae = AutoencoderKL.from_pretrained(args.vae_path).to(dtype=torch.float16)
-    unet = UNet2DConditionModel.from_pretrained('diffusion_1.5/unet').to(dtype=torch.float16)
+    unet = UNet2DConditionModel.from_pretrained('/diffusion_1.5/unet').to(dtype=torch.float16)
     sd_pipe = StableDiffusionPipeline.from_pretrained(
         args.sd_path,
         torch_dtype=torch.float16,
@@ -121,6 +121,38 @@ elif args.sd_version == '1.5plus':
     autostudio = AUTOSTUDIOPlus(sd_pipe, image_encoder_path, ip_ckpt, device, num_tokens=16)
     print('Succesfully load models')
 
+elif args.sd_version == 'xl':
+    base_model_path = "pretrained_models/diffusion_xl"
+    image_encoder_path = "pretrained_models/image_adapter/sdxl_models/image_encoder/"
+    ip_ckpt = "pretrained_models/image_adapter/sdxl_models/ip-adapter_sdxl.bin"
+    unet = UNet2DConditionModel.from_pretrained('pretrained_models/diffusion_xl/unet').to(dtype=torch.float16)
+    vae = AutoencoderKL.from_pretrained('stabilityai/sdxl-vae').to(dtype=torch.float16)
+
+    sd_pipe = StableDiffusionXLPipeline.from_pretrained(
+        base_model_path,
+        unet = unet,
+        vae = vae,
+        torch_dtype=torch.float16,
+        add_watermarker=False,
+    )
+    autostudio = AUTOSTUDIOXL(sd_pipe, image_encoder_path, ip_ckpt, device)
+
+elif args.sd_version == 'xlplus':
+    base_model_path = "pretrained_models/diffusion_xl"
+    image_encoder_path = "/IP-Adapter/models/image_encoder"
+    ip_ckpt = "/IP-Adapter/sdxl_models/ip-adapter-plus_sdxl_vit-h.bin"
+    unet = UNet2DConditionModel.from_pretrained('pretrained_models/diffusion_xl/unet').to(dtype=torch.float16)
+    vae = AutoencoderKL.from_pretrained('stabilityai/sdxl-vae').to(dtype=torch.float16)
+
+    sd_pipe = StableDiffusionXLPipeline.from_pretrained(
+        base_model_path,
+        unet = unet,
+        vae = vae,
+        torch_dtype=torch.float16,
+        add_watermarker=False,
+    )
+    autostudio = AUTOSTUDIOXLPlus(sd_pipe, image_encoder_path, ip_ckpt, device, num_tokens=16)
+
 with open(args.data_path, 'r', encoding='utf-8') as file:
     instructions = json.load(file)
 
@@ -130,7 +162,7 @@ for dialogue in instructions:
     os.makedirs(f"{args.save_dir}/{dialogue}", exist_ok=True)
     start_time = time.time()
 
-    # '0' stand for place holder image. If you need a human-specified reference, please read the image and import it into the corresponding ID, like {,..., '1': img1}
+    # '0' stand for a place holder image. If you need a human-specified reference, please read the image and import it into the corresponding ID, like {,..., '1': img1}
     character_database = {'0':Image.new('RGB', (100, 100), (255, 255, 255))} 
     
     for turn in [f'turn {i+1}' for i in range(15)]:
@@ -195,27 +227,25 @@ for dialogue in instructions:
         for repeat_ind in range(args.repeats):
             ind_offset = repeat_ind * LARGE_CONSTANT2 + args.seed_offset
             vis_location = [dialogue, turn]
-            try:
-                output = autostudio.generate(
-                                            GROUNDING_DINO_MODEL,
-                                            EFFICIENT_SAM_MODEL,
-                                            character_database, 
-                                            prompt_book, 
-                                            do_latent_guidance,
-                                            refine_step=20,
-                                            num_samples=1, num_inference_steps=30, seed=ind_offset,
-                                            img_scale=0.7,
-                                            fuse_scale=[1.2, 0],
-                                            height = prompt_book['size'][0],
-                                            width = prompt_book['size'][1],
-                                            is_editing = False,
-                                            repeat_ind = repeat_ind,
-                                            )
-                output, character_database = output[0][0], output[1]
-                show_image(output, "img", repeat_ind, 25, save_turn_dir) 
-            except:
-               print("ERROR!")
-               break
+
+            output = autostudio.generate(
+                                        GROUNDING_DINO_MODEL,
+                                        EFFICIENT_SAM_MODEL,
+                                        character_database, 
+                                        prompt_book, 
+                                        do_latent_guidance,
+                                        refine_step=20,
+                                        num_samples=1, num_inference_steps=30, seed=ind_offset,
+                                        img_scale=0.7,
+                                        fuse_scale=[1.2, 0],
+                                        height = prompt_book['size'][0],
+                                        width = prompt_book['size'][1],
+                                        is_editing = False,
+                                        repeat_ind = repeat_ind,
+                                        )
+            output, character_database = output[0][0], output[1]
+            show_image(output, "img", repeat_ind, 25, save_turn_dir) 
+
     end_time = time.time()
     use_time = end_time - start_time
     print("single dialogue time:",use_time)
